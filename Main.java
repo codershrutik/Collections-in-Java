dfbdaaf2fbb64d40b98cabb36a40f6926702a31d
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-07-05 10:22:22 +0000

    ArrayList added

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args) {
+        
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Shruti");
+        names.add("John");
+        names.add("William");
+        names.add("Harry");
+        names.add("Louis");
+        names.add("Niel");
+        names.add("Ludwig");
+        System.out.println(names);
+
+    }
+}
