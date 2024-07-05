7d98c32b7508ccda5bbf4c4000c35d88973c00f8
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-07-05 11:08:31 +0000

    Autoboxing and au-unboxing, manual boxing and manual unboxing

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
 import java.util.ArrayList;
 
+class IntWrapper{
+    public int num;
+
+    public int getNum(){
+        return num;
+    }
+    public void setNum(int num){
+        this.num = num;
+    }
+
+    public IntWrapper(int num){
+        this.num = num;
+    }
+}
+
 public class Main{
     public static void main(String[] args) {
-        
-        ArrayList<String> names = new ArrayList<>();
-        names.add("Shruti");
-        names.add("John");
-        names.add("William");
-        names.add("Harry");
-        System.out.println(names.remove("Shruti"));
-        System.out.println(names);
+        ArrayList<Integer> numberList = new ArrayList<>();
+        numberList.add(56); //Autobonxing
+        numberList.add(45);
+        System.out.println(numberList); //Auto-unboxing
+
+        ArrayList<IntWrapper> numList2 = new ArrayList<>();
+        numList2.add(new IntWrapper(34));  //manual-boxing
+        numList2.add(new IntWrapper(23));
+        System.out.println(numList2.get(0).getNum()); 
 
     }
 }
