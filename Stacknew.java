080da5623d57a028c1e388357d13827320db7642
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-07-05 12:42:09 +0000

    Stack methods added

--- Stacknew.java
+++ Stacknew.java
@@ -6,14 +6,16 @@ public class Stacknew {
         Stack<Integer> nums = new Stack<>();
         nums.push(1);
         nums.push(2);
-        nums.push(3);
-        nums.push(4);
-        nums.push(5);
-        System.out.println(nums);
-        System.out.println(nums.isEmpty());
-        System.out.println(nums.search(3));
-        System.out.println(nums.pop());
-        System.out.println(nums);
+        // nums.push(3);
+        // nums.push(4);
+        // nums.push(5);
+        // System.out.println(nums);
+        // System.out.println(nums.isEmpty());
+        // System.out.println(nums.search(3));
+        // System.out.println(nums.pop());
+        // System.out.println(nums);
 
+        int x=nums.search(35);
+        System.out.println(x);
     }
 }
